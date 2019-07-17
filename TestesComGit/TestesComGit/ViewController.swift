1542953ec2383664724edfb6bdcbec61f2c96263
Joao Paulo <joao.auhsey@gmail.com>
2019-07-17 15:15:55 -0300

    creating a new project

---
+++ TestesComGit/TestesComGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestesComGit
+//
+//  Created by Joao Paulo Pereira dos Santos on 17/07/19.
+//  Copyright © 2019 Joao Paulo Pereira dos Santos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
